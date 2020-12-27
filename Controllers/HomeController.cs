a4aaeb7c8720c64d72358a6d084532029c8a42f6
savosin_sergey@mail.ru <savosin_sergey@mail.ru>
2020-12-27 23:37:42 +0300

    RsvpForm added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,37 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Mvc;
 using PartyInvitesAspNetCore.Models;
 
 namespace PartyInvitesAspNetCore.Controllers
 {
-    public class HomeController : Controller
+	public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
+        [HttpGet]
+        public ViewResult RsvpForm()
+		{
             return View();
-        }
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+		{
+            // ToDo: store response
+            return View();
+		}
     }
 }
